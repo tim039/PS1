65c0dc86bc78481ef4acda384810aab8d4d62eed
Timothy <Timothy@128.4.60.112>
2016-09-03 17:30:55 -0400

    second commit

--- src/pkgMain/Rectangle.java
+++ src/pkgMain/Rectangle.java
@@ -18,6 +18,6 @@ public class Rectangle {
 	public double Area()
 	{
 		// TODO Implement Area() function with the correct formula
-		return Length * Width;
+		return Length * Width; // testing
 	}	
 }
